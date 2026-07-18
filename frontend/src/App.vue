08d590d203dbdb83486a51652c3baa67ed14cd8f
stefan.genie <stefanf1337@gmail.com>
2026-07-18 16:13:46 +0300

    feat(frontend): shareable links with embedded access code (?code=...)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { onMounted } from "vue";
+import { setAccessCode } from "./utils/access.js";
+
+onMounted(() => {
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+  if (code) {
+    setAccessCode(code);
+    const url = window.location.pathname + window.location.hash;
+    window.history.replaceState({}, document.title, url);
+  }
+});
+</script>
+
 <template>
   <router-view />
 </template>
