5a7f85b822934502c53501903acd2240e7c11f44
stefan.genie <stefanf1337@gmail.com>
2026-07-18 16:15:20 +0300

    dev(env): flake.nix, envrc and .gitignore

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Kubernetes development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pnpm
+            nodejs_24
+          ];
+
+          shellHook = ''
+            echo "⚡ Kubernetes devshell"
+          '';
+        };
+      });
+}
